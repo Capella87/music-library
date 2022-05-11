07791f49b9d311f7b02e51a920e4897c0fd16c85
Capella87 <contact@capelladev.com>
2022-05-12 01:48:22 +0900

    Initial commit

---
+++ music-library/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace MusicLibrary
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My Music Library");
+        }
+    }
+}
