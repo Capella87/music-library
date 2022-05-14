02e4b8f7530a007bfbc53f3ce1219630bd2c6e35
Capella87 <contact@capelladev.com>
2022-05-15 03:09:38 +0900

    Add StateEnums.cs

---
+++ music-library/StateEnums.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.State
+{
+    /// <summary>
+    /// Define Scan type.
+    /// </summary>
+    enum ScanType
+    {
+        FullScan,
+        NewFileOnly,
+    };
+}
