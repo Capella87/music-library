115ceeb7b55ace77cca0fa216fde1779c6211632
Capella87 <contact@capelladev.com>
2022-05-17 16:20:28 +0900

    Add argument handling

--- music-library/Program.cs
+++ music-library/Program.cs
@@ -13,9 +13,55 @@ namespace MusicLibrary
         private static string _dbPath = @"\";
         private static string _dbName = @"musiclibrary.db";
 
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            var myLibrary = new MusicLibrary.Library(Directory.GetCurrentDirectory(), _dbName);
+            return await Run(args);
+        }
+
+        public static async Task<int> Run(string[] args)
+        {
+            return await ParseCommand(args);
+        }
+
+        private static async Task<int> ParseCommand(string[] args)
+        {
+            var rootCommand = new RootCommand("A music library implementation in C#")
+            {
+
+            };
+
+            var listCommand = new Command("list", "Show entries in the library.")
+            {
+
+            };
+            
+            var updateCommand = new Command("update", "Update database in specified directries.")
+            {
+
+            };
+
+            var importCommand = new Command("import", "Import a file, playlist or files in specific directory.")
+            {
+
+            };
+
+            var removeCommand = new Command("remove", "Remove an entry or specified directory from the library.")
+            {      
+
+            };
+
+            var searchCommand = new Command("search", "Search database.")
+            {
+
+            };
+
+            rootCommand.AddCommand(listCommand);
+            rootCommand.AddCommand(updateCommand);
+            rootCommand.AddCommand(importCommand);
+            rootCommand.AddCommand(removeCommand);
+            rootCommand.AddCommand(searchCommand);
+
+            return await rootCommand.InvokeAsync(args);
         }
     }
 }
