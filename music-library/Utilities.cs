5c14e05d9f91065aec2bddbf65398cd61e33bb7d
Capella87 <contact@capelladev.com>
2022-05-20 22:48:52 +0900

    Add GetModifiedTime()

--- music-library/Utilities.cs
+++ music-library/Utilities.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Data.Sqlite;
+
 namespace MusicLibrary.Utilities
 {
     public static class PathTools
@@ -31,5 +33,47 @@ namespace MusicLibrary.Utilities
             rt.Append(fileName);
             return rt.ToString();
         }
+    }
+
+    public static class FileTools
+    {
+        /// <summary>
+        /// Get last modified date of targeted files.
+        /// </summary>
+        /// <param name="rt">Dictionary which contains file and its modified time. out parameter.</param>
+        /// <param name="files">File list to get modified time.</param>
+        /// <returns>A list containing failed Uris to get modified time.</returns>
+        public static List<Uri> GetModifiedTime(out Dictionary<Uri, DateTime> rt, List<Uri> files)
+        {
+            rt = new Dictionary<Uri, DateTime>();
+            List<Uri> failed = new();
+
+
+            foreach (var e in files)
+            {
+                try
+                {
+                    var modifiedTime = File.GetLastAccessTimeUtc(e.LocalPath);
+                    rt.Add(e, modifiedTime);
+                }
+                catch (UnauthorizedAccessException err)
+                {
+                    // Logging or print error information in the console.
+
+                    // Add to failed;
+                    failed.Add(e);
+                }
+                catch (NotSupportedException err)
+                {
+                    // Logging or print error information in the console.
+
+                    // Add to failed
+                    failed.Add(e);
+                }
+            }
+
+            return failed;
+        }
+
     }
 }
