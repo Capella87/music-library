ab407a8a81b34f6fb52c4ecc75803d97f1c00934
Capella87 <contact@capelladev.com>
2022-05-20 22:48:30 +0900

    Update ScanType enum

--- music-library/StateEnums.cs
+++ music-library/StateEnums.cs
@@ -8,14 +8,16 @@ namespace MusicLibrary.State
 {
     /// <summary>
     /// Define Scan type.
+    /// FullScan : Full scan; Usually it's used to update database;
+    /// NewEntryScan : Only adding new files or directories. "import" command uses this.
     /// </summary>
-    enum ScanType
+    public enum ScanType
     {
         FullScan,
-        NewFileOnly,
+        NewEntryScan,
     };
 
-    enum FileType
+    public enum FileType
     {
         Music,
         Playlist,
