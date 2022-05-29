fb73b24c5fd87f960bd845293a448ad7649887a3
Capella87 <contact@capelladev.com>
2022-05-29 18:18:18 +0900

    Add ask interactive for reset

--- music-library/Reset.cs
+++ music-library/Reset.cs
@@ -15,24 +15,51 @@ namespace MusicLibrary.Commands
         public static Task ResetDatabase(string _dbPath, string _dbName)
         {
             var library = new Database.Library(_dbPath, _dbName, true);
+            char answer;
 
-            try
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("WARNING: This command will OBLITERATE ALL your data in the database.");
+            Console.ResetColor();
+            Console.Write("Are you sure to RESET your database [Y/N] : ");
+
+            do
             {
-                using (var connection = library.DBConnection)
+                try
                 {
-                    if (connection == null) throw new NullReferenceException("Database connection is wrong.");
-                    library.Reset();
-                }
-            }
-            catch (NullReferenceException e)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine(e.Message);
-                Console.ResetColor();
+                    answer = Char.Parse(Console.ReadLine());
+                    if (answer == 'Y' || answer == 'y')
+                    {
+                        Console.WriteLine("Reset all of your data in database. All files will NOT be affected.");
+                        try
+                        {
+                            using (var connection = library.DBConnection)
+                            {
+                                if (connection == null) throw new NullReferenceException("Database connection is wrong.");
+                                library.Reset();
+                            }
+                        }
+                        catch (NullReferenceException e)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine(e.Message);
+                            Console.ResetColor();
 
-                Console.WriteLine(e.StackTrace);
-                return Task.FromResult(1);
-            }
+                            Console.WriteLine(e.StackTrace);
+                            return Task.FromResult(1);
+                        }
+                        break;
+                    }
+                    else if (answer == 'n' || answer == 'n')
+                    {
+                        Console.WriteLine("Abort.");
+                        break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Wrong input.");
+                }
+            } while (true);
 
             return Task.FromResult(0);
         }
