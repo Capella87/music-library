579c4432cb032e05d521cee614a795785cbf9bc3
Capella87 <contact@capelladev.com>
2022-06-12 23:32:45 +0900

    Add Configuration.cs

---
+++ music-library/config/Configuration.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration.Json;
+
+namespace MusicLibrary.Config
+{
+    public sealed class Configuration
+    {
+        // public RuntimeEnvironment RuntimeEnvironment { get; set; };
+    }
+}
