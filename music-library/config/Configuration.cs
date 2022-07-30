e3dd690798b2ec7de994f116a3047e58fffcdf7b
Capella87 <contact@capelladev.com>
2022-07-30 18:26:25 +0900

    Add configuration classes

--- music-library/config/Configuration.cs
+++ music-library/config/Configuration.cs
@@ -8,8 +8,48 @@ using Microsoft.Extensions.Configuration.Json;
 
 namespace MusicLibrary.Config
 {
-    public sealed class Configuration
+    namespace UserSettings
     {
-        // public RuntimeEnvironment RuntimeEnvironment { get; set; };
+        public class Library
+        {
+            public string[]? Directories { get; set; }
+            public string[]? Files { get; set; }
+
+            public string? DatabaseName { get; set; }
+            public string? DatabasePath { get; set; }
+
+            public bool EnableScanSubdirectories { get; set; }
+            public bool EnableAutoUpdateLibrary { get; set; }
+        }
+
+        public class Logging
+        {
+            public string? LogFilePath { get; set; }
+        }
+
+        public class Player
+        {
+            public string? DefaultPlayer { get; set; }
+            public PlayerEntries? Players { get; set; }
+        }
+
+        public class PlayerEntries
+        {
+            public string? Name { get; set; }
+            public string? Description { get; set; }
+            public string? PlayerPath { get; set; }
+            public Arguments? Args { get; set; }
+        }
+
+        public class Arguments
+        {
+            public string? Option { get; set; }
+            public string? ValueType { get; set; }
+            public string? Value { get; set; }
+        }
+    }
+
+    namespace DefaultSettings
+    {
     }
 }
